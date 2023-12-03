5bcdcb08bc38f175f87151e5704640d437bc43b8
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-12-03 07:29:24 -0600

    maybe fixed, check in class

--- src/main/java/edu/francistuttle/App.java
+++ src/main/java/edu/francistuttle/App.java
@@ -3,16 +3,16 @@ package edu.francistuttle;
 import org.w3c.dom.*;
 import javax.xml.parsers.*;
 import java.io.*;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
         ComputerLab lab = new ComputerLab();
-
-        for(int comp = 0; comp < lab.parse().size(); comp++)
-        {
-            System.out.println(lab.parse().get(comp));
+        ArrayList<Computer> computer = new ArrayList<Computer>();
+        computer = lab.parse();
+        for (int comp = 0; comp < computer.size(); comp++) {
+            System.out.println(computer.get(comp));
         }
 
-        
     }
 }
