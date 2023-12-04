caef495b37d7debf62b20083949e639dd497415b
AbeKhan <128449118+AbeKhan@users.noreply.github.com>
2023-12-04 14:19:48 -0600

    pushing

--- src/main/java/edu/francistuttle/App.java
+++ src/main/java/edu/francistuttle/App.java
@@ -8,8 +8,7 @@ import java.util.ArrayList;
 public class App {
     public static void main(String[] args) {
         ComputerLab lab = new ComputerLab();
-        ArrayList<Computer> computer = new ArrayList<Computer>();
-        computer = lab.parse();
+        ArrayList<Computer> computer = lab.parse();
         for (int comp = 0; comp < computer.size(); comp++) {
             System.out.println(computer.get(comp));
         }
